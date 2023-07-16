5411c2557b9c6e9f5f4b44b1ebe1d0b867d096ae
1Vaibhav-Mishra <90379095+1Vaibhav-Mishra@users.noreply.github.com>
2023-07-16 20:31:16 +0530

    amazon clone

--- app.js
+++ app.js
@@ -1,46 +1,92 @@
-const express = require("express");
-const bodyparser = require("body-parser");
-const date = require(__dirname + "/date.js");
-console.log(date.getDay());
-const app = express();
-let items = [];
-let workitems = [];
-app.set("view engine", "ejs");
-app.use(bodyparser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
-app.get("/", function(req, res) {
-  let day = date.getDate();
-  res.render("list", {
-    listTitle: day,
-    newitems: items
-  });
-});
-app.post("/", function(req, res) {
-  let item = req.body.newitem;
-  if (req.body.list === "work") {
-    workitems.push(item);
-    res.redirect("/work");
-  } else
+const leftBtn= document.querySelector(".l-btn");
+const rightBtn = document.querySelector(".r-btn");
 
-  {
-    items.push(item);
-    res.redirect("/");
-  }
-});
-app.get("/work", function(req, res) {
 
-  res.render("list", {
-    listTitle: "Work List",
-    newitems: workitems
-  });
-});
-app.post("/work", function(req, res) {
-  let item = req.body.newitem;
-  workitems.push(item);
-  res.redirect("/work");
-});
-app.listen(3000, function() {
-  console.log("server is running");
-});
+rightBtn.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide");
+        conent.scrollLeft +=1100;
+        event.preventDefault();
+
+})
+leftBtn.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide");
+        conent.scrollLeft -=1100;
+        event.preventDefault();
+
+})
+const leftBtn1= document.querySelector(".btn-1b");
+const rightBtn1 = document.querySelector(".btn-1a");
+
+
+rightBtn1.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-1");
+        conent.scrollLeft +=1100;
+        event.preventDefault();
+
+})
+leftBtn1.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-1");
+        conent.scrollLeft -=1100;
+        event.preventDefault();
+
+})
+const leftBtn2= document.querySelector(".btn-1c");
+const rightBtn2 = document.querySelector(".btn-1d");
+
+
+rightBtn2.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-2");
+        conent.scrollLeft +=1100;
+        event.preventDefault();
+
+})
+leftBtn2.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-2");
+        conent.scrollLeft -=1100;
+        event.preventDefault();
+
+})
+
+const backtop = document.querySelector(".backtop");
+
+backtop.addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+})
+
+const sidebar=document.querySelector(".sidebar");
+const cross=document.querySelector(".fa-xmark");
+const black=document.querySelector(".black");
+const sidebtn=document.querySelector(".second-1");
+
+sidebtn.addEventListener("click",()=>{
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+    document.body.classList.add("stop-scroll");
+})
+cross.addEventListener("click",()=>{
+    sidebar.classList.remove("active");
+    cross.classList.remove("active");
+    black.classList.remove("active");
+    document.body.classList.remove("stop-scroll");
+})
+
+const sign=document.querySelector(".ac");
+const tri=document.querySelector(".triangle");
+const signin=document.querySelector(".hdn-sign");
+
+sign.addEventListener("click",()=>{
+    black.classList.toggle("active-1");
+    signin.classList.toggle("active");
+    tri.classList.toggle("active");
+    document.body.classList.toggle("stop-scroll");
+})
